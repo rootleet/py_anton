60971de0b783d050a447d121ef3387a5392c3bcf
rootleet <Sunderland@411>
2021-06-22 08:09:04 +0000

    Vigenere Ecnryption added

--- modules/tool.py
+++ modules/tool.py
@@ -1,10 +1,16 @@
+import datetime
+
 from termcolor import colored
 import hashlib
 from os import system, name
 import qrcode
 from PIL import Image
+import re
+import string
+
+
 def home():
-    x=0
+    x = 0
     while x < 4:
         y = x
         print(colored("01" * 9, 'grey', 'on_red'), colored("               ", 'grey', 'on_red'),
@@ -42,6 +48,7 @@ def home():
     print(('-' * 50))
     print()
 
+
 def make_md5(my_string):
     m = hashlib.md5()
     m.update(my_string.encode('utf-8'))
@@ -65,6 +72,8 @@ def anton(invoke='None'):
         print(colored("Trigger  Name                    Description", 'red', "on_blue", attrs=['bold']))
         print()
         print(colored("1        Make Qr Code             Make QR Code that contains data", 'red', attrs=['bold']))
+        print(colored("2        vigenere cipher          A python code that performs a Vigenere cipher.", 'red',
+                      attrs=['bold']))
         print(colored("0        Quite                    Exist tool", 'red', attrs=['bold']))
         print()
         tool = input(colored("Select an Option : ", 'blue'))
@@ -72,9 +81,10 @@ def anton(invoke='None'):
         clear()
         tool = invoke
 
+    # exit
     if tool == '0':
         quit("Good By Horse....")
-
+    # qr code
     elif tool == '1':
         clear()
         import qrcode
@@ -110,12 +120,109 @@ def anton(invoke='None'):
     elif tool == 'x':
         clear()
         anton("None")
-
+    # Vigenere cipher.
+    elif tool == '2':
+        vigenere_cipher()
     else:
         clear()
         print("Option Unknown")
         tool = input(colored("Select an Option : ", 'blue'))
         anton(tool)
+
+
+# vizier cypher
+def vigenere_cipher():
+    alphabets = "abcdefghijklmnopqrstuvwxyz"  # this is the english letters
+
+    def encrypt(p, k):
+        c = ""
+        kpos = []  # return the index of characters ex: if k='d' then kpos= 3
+        for x in k:
+            # kpos += alphabets.find(x) #change the int value to string
+            kpos.append(alphabets.find(x))
+        i = 0
+        for x in p:
+            if i == len(kpos):
+                i = 0
+            pos = alphabets.find(x) + kpos[
+                i]  # find the number or index of the character and perform the shift with the key
+            # print(pos)
+            if pos > 25:
+                pos = pos - 26  # check you exceed the limit
+            c += alphabets[pos].capitalize()  # because the cipher text always capital letters
+            i += 1
+        func = "Encrypt"
+        plain_text = p
+        key = k
+        encrypted = c
+        print("#" * 10, "\nFunction : ", func, "\nMessage : ", plain_text, "\nKey : ", key, "\nEncryption : ",
+              encrypted)
+        result = "###### Function : " +str(func) + " Message : " + str(plain_text) + " Key : " + str(
+            key) + " Encryption : " + str(encrypted) + "\n"
+        print("#" * 10)
+        f = open("assets/encryptions/encrypt.txt", "a+")
+        f.write(str(result))
+        f.close()
+        return c
+
+    def decrypt(c, k):
+        p = ""
+        kpos = []
+        for x in k:
+            kpos.append(alphabets.find(x))
+        i = 0
+        for x in c:
+            if i == len(kpos):
+                i = 0
+            pos = alphabets.find(x.lower()) - kpos[i]
+            if pos < 0:
+                pos = pos + 26
+            p += alphabets[pos].lower()
+            i += 1
+        return p
+
+    try:
+        print("Welcome to vigenere cipher.\n\n"
+              "The text message should contain only characters and the key should be one character word \n"
+              "Press 1 to Enrypt a message \npress 2 to Decrypt a message \npress 0 to return home")
+        choose = input("Choice: ")
+        if choose == '1':
+            p = input("enter the plain text: ")
+            p = p.replace(" ", "")  # this will make sure that there is no space in the message
+            if p.isalpha():
+                k = input("Enter the key: ")
+                k = k.strip()  # remove the white spaces from both sides
+                if k.isalpha():
+                    # print(k)
+                    c = encrypt(p, k)
+                    vigenere_cipher()
+
+                else:
+                    print(k)
+                    print("Enter valid key, key is only one character word!")
+            else:
+                print("only letters are allowed !!")
+
+        elif choose == '2':
+            c = input("enter the cipher text: ")
+            c = c.replace(" ", "")
+            if c.isalpha():
+                k = input("Enter the key: ")
+                if not k.isalpha():
+                    print("Enter valid key, key is only one character word!")
+                else:
+                    p = decrypt(c, k)
+                    print("The plain text is: ", p)
+            else:
+                print("only letters are allowed!")
+
+        elif choose == '0':
+            anton()
+        else:
+            print("Please enter a valid choice!")
+    except Exception as e:
+        print(e)
+        exit("Enter a valid text please! ")
 
 
 anton()
